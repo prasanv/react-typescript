dd44dd9f61bb240a9b9c6743b2cf18540467b224
prasanv <prasan.venkatb@gmail.com>
2022-07-12 00:24:58 -0700

    add page links

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,68 @@
 import type { NextPage } from 'next'
+import Link from 'next/link'
+
 
 const Home: NextPage = () => {
 
   
   return (
-    <>
+    <div>
       <h2>React Typescript</h2>
-    </>
+          <Link href='/1-basic_prop_type'>
+            <a>1-basic_prop_type</a>
+          </Link><br></br>
+          <Link href='/2-array_prop_type'>
+            <a>2-array_prop_type</a>
+          </Link><br></br>
+          <Link href='/3-object_prop_type'>
+            <a>3-object_prop_type</a>
+          </Link><br></br>
+          <Link href='/4-react_css_prop_type'>
+            <a>4-react_css_prop_type</a>
+          </Link><br></br>
+          <Link href='/5-react_event_prop_type'>
+            <a>5-react_event_prop_type</a>
+          </Link><br></br>
+          <Link href='/6-react_node_prop_type'>
+            <a>6-react_node_prop_type</a>
+          </Link><br></br>
+          <Link href='/7-usestate_prop_type'>
+            <a>7-usestate_prop_type</a>
+          </Link><br></br>
+          <Link href='/8-usereducer_prop_type'>
+            <a>8-usereducer_prop_type</a>
+          </Link><br></br>
+          <Link href='/9-usecontext_prop_type'>
+            <a>9-usecontext_prop_type</a>
+          </Link><br></br>
+          <Link href='/10-usecontext_future_prop_type'>
+            <a>10-usecontext_future_prop_type</a>
+          </Link><br></br>
+          <Link href='/11-useref_prop_type'>
+            <a>11-useref_prop_type</a>
+          </Link><br></br>
+          <Link href='/12-react_component_prop_type'>
+            <a>12-react_component_prop_type</a>
+          </Link><br></br>
+          <Link href='/13-generics'>
+            <a>13-generics</a>
+          </Link><br></br>
+          <Link href='/14-restricting_prop_type'>
+            <a>14-restricting_prop_type</a>
+          </Link><br></br>
+          <Link href='/15-temp_literals_and_exclude_type'>
+            <a>15-temp_literals_and_exclude_type</a>
+          </Link><br></br>
+          <Link href='/16-html_comp_prop_type_and_omit'>
+            <a>16-html_comp_prop_type_and_omit</a>
+          </Link><br></br>
+          <Link href='/17-extract_comp_prop_types'>
+            <a>17-extract_comp_prop_types</a>
+          </Link><br></br>
+          <Link href='/18-polymorphic_comp'>
+            <a>18-polymorphic_comp</a>
+          </Link><br></br>
+    </div>
   )
 }
 
