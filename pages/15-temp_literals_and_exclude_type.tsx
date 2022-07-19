9d792b94871e0bf7a0bae53550626304ffff40da
prasanv <34255378+prasanv@users.noreply.github.com>
2022-07-18 22:46:09 -0700

    update components (#1)

--- pages/15-temp_literals_and_exclude_type.tsx
+++ pages/15-temp_literals_and_exclude_type.tsx
@@ -2,11 +2,7 @@ import React from 'react'
 import Toast from '../src/components/15-temp_literals_and_exclude_type/Toast'
 
 const Temp_literals_and_exclude_type = () => {
-  return (
-    <div>
-      <Toast position={'center'}></Toast>
-    </div>
-  )
+  return (<Toast position={'center'}></Toast>)
 }
 
 export default Temp_literals_and_exclude_type
